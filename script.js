34c66f4361753940d7fe803286c58d16906ea409
julienlegault <julien.p.legault@gmail.com>
2023-10-16 15:20:59 -0500

    Clean up js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const victoryConditions = [
+   [0,1,2,3,4],
+   [5,6,7,8,9],
+   [10,11,12,13,14],
+   [15,16,17,18,19],
+   [20,21,22,23,24],
+   [0,5,10,15,20],
+   [1,6,11,16,21],
+   [2,7,12,17,22],
+   [4,9,14,19,24],
+   [3,8,13,18,23],
+   [4,8,12,16,20],
+   [0,6,12,18,24],
+];
+
 var coloredArray = [];
 var victoryArray = [];
 var arrayOfPhrases = [];
@@ -56,34 +71,20 @@ function updateBackgroundColor(cell, color){
 }
 
 function checkVictory() {
-   if(test([0,1,2,3,4])) { greeny([0,1,2,3,4]) }
-   if(test([5,6,7,8,9])) { greeny([5,6,7,8,9]) }
-   if(test([10,11,12,13,14])) { greeny([10,11,12,13,14]) }
-   if(test([15,16,17,18,19])) { greeny([15,16,17,18,19]) }
-   if(test([20,21,22,23,24])) { greeny([20,21,22,23,24]) }
-   if(test([0,5,10,15,20])) { greeny([0,5,10,15,20]) }
-   if(test([1,6,11,16,21])) { greeny([1,6,11,16,21]) }
-   if(test([2,7,12,17,22])) { greeny([2,7,12,17,22]) }
-   if(test([3,8,13,18,23])) { greeny([3,8,13,18,23]) }
-   if(test([4,9,14,19,24])) { greeny([4,9,14,19,24]) }
-   if(test([0,6,12,18,24])) { greeny([0,6,12,18,24]) }
-   if(test([4,8,12,16,20])) { greeny([4,8,12,16,20]) }
-}
-
-function greeny(array) {
-   victoryArray.push(...array);
-   for(var x of array){
-      updateBackgroundColor(document.getElementById(x), 'green');
+   for(var array of victoryConditions) {
+      let victoryAchieved = true;
+      for(var x of array){
+         if(!coloredArray.includes(x)){
+            victoryAchieved = false;
+         }  
+      }
+      if (victoryAchieved){
+         victoryArray.push(...array);
+         for(var x of array){
+            updateBackgroundColor(document.getElementById(x), 'green');
+         }
+      }
    }
-}
-
-function test(array) {
-   for(var x of array){
-      if(!coloredArray.includes(x)){
-         return false;
-      }  
-   }
-   return true;
 }
 
 function savePhrases() {
